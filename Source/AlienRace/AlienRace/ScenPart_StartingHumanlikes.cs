9ed76e51dd09fcd21cf6e19a178f689e821ee6f7
erdelf <erdelf.productions@gmail.com>
2017-02-25 21:08:53 +0100

    added new scen comp to add humanlikes

--- Source/AlienRace/AlienRace/ScenPart_StartingHumanlikes.cs
+++ Source/AlienRace/AlienRace/ScenPart_StartingHumanlikes.cs
@@ -11,7 +11,7 @@ namespace AlienRace
     [StaticConstructorOnStartup]
     class ScenPart_StartingHumanlikes : ScenPart
     {
-        /*
+        
         static ScenPart_StartingHumanlikes()
         {
             ScenPartDef scenPart = new ScenPartDef()
@@ -28,30 +28,61 @@ namespace AlienRace
             DefDatabase<ScenPartDef>.Add(scenPart);
         }
 
+        PawnKindDef kindDef = PawnKindDefOf.Villager;
+        int pawnCount = 0;
+
+        string buffer;
+
         public override void DoEditInterface(Listing_ScenEdit listing)
         {
             Rect scenPartRect = listing.GetScenPartRect(this, ScenPart.RowHeight * 3f);
-            if (Widgets.ButtonText(scenPartRect.TopPart(0.333f), this.trait.DataAtDegree(this.degree).label.CapitalizeFirst(), true, false, true))
+            if (Widgets.ButtonText(scenPartRect.TopPart(0.45f), this.kindDef.label.CapitalizeFirst(), true, false, true))
             {
                 List<FloatMenuOption> list = new List<FloatMenuOption>();
-                foreach (TraitDef current in from td in DefDatabase<TraitDef>.AllDefs
-                                             orderby td.label
-                                             select td)
-                {
-                    foreach (TraitDegreeData current2 in current.degreeDatas)
-                    {
-                        TraitDef localDef = current;
-                        TraitDegreeData localDeg = current2;
-                        list.Add(new FloatMenuOption(localDeg.label.CapitalizeFirst(), delegate
-                        {
-                            this.trait = localDef;
-                            this.degree = localDeg.degree;
-                        }, MenuOptionPriority.Default, null, null, 0f, null, null));
-                    }
-                }
+                list.AddRange(DefDatabase<ThingDef_AlienRace>.AllDefsListForReading.Where(ar => ar.alienRace.pawnKindSettings.startingColonists != null).SelectMany(ar => ar.alienRace.pawnKindSettings.startingColonists.SelectMany(ste => ste.pawnKindEntries.SelectMany(pke => pke.kindDefs))).Select(pkd => new FloatMenuOption(pkd.label.CapitalizeFirst(), () => kindDef = pkd)));
+                list.Add(new FloatMenuOption("Villager", () => kindDef = PawnKindDefOf.Villager));
                 Find.WindowStack.Add(new FloatMenu(list));
             }
-            base.DoPawnModifierEditInterface(scenPartRect.BottomPart(0.666f));
-        }*/
+            Widgets.TextFieldNumeric<int>(scenPartRect.BottomPart(0.45f), ref pawnCount, ref buffer, 0);
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.LookValue<int>(ref pawnCount, "alienRaceScenPawnCount", 0);
+            Scribe_Defs.LookDef<PawnKindDef>(ref kindDef, "PawnKindDefAlienRaceScen");
+        }
+
+        public override string Summary(Scenario scen)
+        {
+            return ScenSummaryList.SummaryWithList(scen, "PlayerStartsWith", ScenPart_StartingThing_Defined.PlayerStartWithIntro);
+        }
+
+        public override IEnumerable<string> GetSummaryListEntries(string tag)
+        {
+            if (tag == "PlayerStartsWith")
+                yield return kindDef.LabelCap + " x" + pawnCount;
+            yield break;
+        }
+
+        public override bool TryMerge(ScenPart other)
+        {
+            ScenPart_StartingHumanlikes others = other as ScenPart_StartingHumanlikes;
+            if (others == null || others.kindDef != this.kindDef)
+                return false;
+            this.pawnCount += others.pawnCount;
+            return true;
+        }
+
+        public override IEnumerable<Thing> PlayerStartingThings()
+        {
+            for (int i = 0; i < this.pawnCount; i++)
+            {
+                Pawn newPawn = PawnGenerator.GeneratePawn(new PawnGenerationRequest(this.kindDef, Faction.OfPlayer, PawnGenerationContext.PlayerStarter, null, true, false, false, false, true, false, 26f, true, true, true, p => DefDatabase<WorkTypeDef>.AllDefsListForReading.Where(wtd => wtd.requireCapableColonist).ToList().TrueForAll(w => !p.story.WorkTypeIsDisabled(w))));
+                Find.GameInitData.startingPawns.Add(newPawn);
+                yield return newPawn;
+            }
+            yield break;
+        }
     }
 }
