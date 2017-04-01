5929ca52ca2efb5878ab63e0ebd036ce290ee0fa
erdelf <erdelf.productions@gmail.com>
2017-04-01 21:25:27 +0200

    added generic thinknode for race checking

---
+++ Source/AlienRace/AlienRace/Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace AlienRace
+{
+    public class ThinkNode_ConditionalIsMemberOfRace : ThinkNode_Conditional
+    {
+        public List<string> races;
+
+        protected override bool Satisfied(Pawn pawn) => races.Contains(pawn.def.defName);
+    }
+}
