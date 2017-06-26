eb7f5732447f455e524e89716b611b6d61794b1b
erdelf <erdelf.productions@gmail.com>
2017-06-27 01:57:08 +0200

    fix for too early config error check in pawnBios

--- Source/AlienRace/AlienRace/PawnBioDef.cs
+++ Source/AlienRace/AlienRace/PawnBioDef.cs
@@ -19,18 +19,13 @@ namespace AlienRace
         public bool factionLeader;
 
 
-        public override IEnumerable<string> ConfigErrors()
-        {
-            foreach (string s in base.ConfigErrors())
-                yield return s;
-            if (BackstoryDatabase.TryGetWithIdentifier(this.childhoodDef, out this.resolvedChildhood))
-                yield return "Childhood backstory not found";
-            if (BackstoryDatabase.TryGetWithIdentifier(this.adulthoodDef, out this.resolvedAdulthood))
-                yield return "Adulthood backstory not found";
-        }
-
         public override void ResolveReferences()
         {
+            if (BackstoryDatabase.TryGetWithIdentifier(this.childhoodDef, out this.resolvedChildhood))
+                Log.Error("Error in " + this.defName + ": Childhood backstory not found");
+            if (BackstoryDatabase.TryGetWithIdentifier(this.adulthoodDef, out this.resolvedAdulthood))
+                Log.Error("Error in " + this.defName + ": Adulthood backstory not found");
+
             base.ResolveReferences();
 
             if (this.resolvedAdulthood.slot != BackstorySlot.Adulthood || this.resolvedChildhood.slot != BackstorySlot.Childhood)
