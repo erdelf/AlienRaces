bb1607bccc0535f4e0d47141f630eb336177bcd0
erdelf <erdelf.productions@gmail.com>
2017-09-06 19:28:01 +0200

    fix for human heads

--- Source/AlienRace/AlienRace/HarmonyPatches.cs
+++ Source/AlienRace/AlienRace/HarmonyPatches.cs
@@ -126,7 +126,7 @@ namespace AlienRace
         static BodyPartDef headPawnDef;
 
         public static void HasHeadPostfix(BodyPartRecord x, bool __result) => 
-            __result = x.def == headPawnDef;
+            __result = headPawnDef != null ? x.def == headPawnDef : __result;
 
         public static void GenerateInitialHediffsPostfix(Pawn pawn) =>
             pawn.story?.AllBackstories?.Select(bs => DefDatabase<BackstoryDef>.GetNamedSilentFail(bs.identifier)).OfType<BackstoryDef>().SelectMany(bd => bd.forcedHediffs).Concat(bioReference?.forcedHediffs ?? new List<string>(0)).Select(s =>
