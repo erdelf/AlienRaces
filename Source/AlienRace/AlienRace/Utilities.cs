255599d3d5c0f0c0af83eddc8b6d7b4570ca86aa
erdelf <erdelf.productions@gmail.com>
2017-09-29 06:46:15 +0200

    xenophobia updated for next test run

--- Source/AlienRace/AlienRace/Utilities.cs
+++ Source/AlienRace/AlienRace/Utilities.cs
@@ -22,7 +22,7 @@ namespace AlienRace
     public class Thought_XenophobeVsXenophile : Thought_SituationalSocial
     {
         public override float OpinionOffset() =>
-             -30f;
+             this.pawn.story.traits.DegreeOfTrait(AlienDefOf.Xenophobia) == 1 ? -25f : -15f;
     }
 
     public class ThoughtWorker_XenophobeVsXenophile : ThoughtWorker
@@ -35,17 +35,31 @@ namespace AlienRace
     public class Thought_XenophobiaVsAlien : Thought_SituationalSocial
     {
         public override float OpinionOffset() => 
-            this.pawn.def != OtherPawn().def ? this.pawn.story.traits.DegreeOfTrait(AlienDefOf.Xenophobia) == 1 || 
-            this.OtherPawn().story.traits.DegreeOfTrait(AlienDefOf.Xenophobia) == 1 ? -30 : 30 : 0;
+            this.pawn.def != OtherPawn().def ? this.pawn.story.traits.DegreeOfTrait(AlienDefOf.Xenophobia) == 1 ? -30 : 
+            this.OtherPawn().story.traits.DegreeOfTrait(AlienDefOf.Xenophobia) == 1 ? -15 : 0 : 0;
     }
 
     public class ThoughtWorker_XenophobiaVsAlien : ThoughtWorker
     {
-        protected override ThoughtState CurrentSocialStateInternal(Pawn p, Pawn otherPawn) => 
-            p.def != otherPawn.def && p.RaceProps.Humanlike && otherPawn.RaceProps.Humanlike && (p.story.traits.HasTrait(AlienDefOf.Xenophobia) || otherPawn.story.traits.HasTrait(AlienDefOf.Xenophobia)) && RelationsUtility.PawnsKnowEachOther(p, otherPawn) ? 
-                p.story.traits.DegreeOfTrait(AlienDefOf.Xenophobia) == 1 || otherPawn.story.traits.DegreeOfTrait(AlienDefOf.Xenophobia) == 1 ? 
-                    ThoughtState.ActiveAtStage(1) : 
-                    ThoughtState.ActiveAtStage(0) : 
+        protected override ThoughtState CurrentSocialStateInternal(Pawn p, Pawn otherPawn) =>
+            p.def != otherPawn.def && p.RaceProps.Humanlike && otherPawn.RaceProps.Humanlike && RelationsUtility.PawnsKnowEachOther(p, otherPawn) ?
+                p.story.traits.HasTrait(AlienDefOf.Xenophobia) ?
+                    p.story.traits.DegreeOfTrait(AlienDefOf.Xenophobia) == -1 ?
+                        ThoughtState.ActiveAtStage(0) :
+                        ThoughtState.ActiveAtStage(1) :
+                    false :
+                false;
+    }
+
+    public class ThoughtWorker_AlienVsXenophobia : ThoughtWorker
+    {
+        protected override ThoughtState CurrentSocialStateInternal(Pawn p, Pawn otherPawn) =>
+            p.def != otherPawn.def && p.RaceProps.Humanlike && otherPawn.RaceProps.Humanlike && RelationsUtility.PawnsKnowEachOther(p, otherPawn) ?
+                otherPawn.story.traits.HasTrait(AlienDefOf.Xenophobia) ?
+                    otherPawn.story.traits.DegreeOfTrait(AlienDefOf.Xenophobia) == -1 ?
+                        ThoughtState.ActiveAtStage(0) :
+                        ThoughtState.ActiveAtStage(1) :
+                    false :
                 false;
     }
 }
