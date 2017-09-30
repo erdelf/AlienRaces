96ea48399626d7cc9238f2dd4868ad27cd8523ff
erdelf <erdelf.productions@gmail.com>
2017-09-30 13:43:56 +0200

    moved BodyAddons to it's own file

--- Source/AlienRace/AlienRace/AlienPartGenerator.cs
+++ Source/AlienRace/AlienRace/AlienPartGenerator.cs
@@ -6,11 +6,10 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine;
 using Verse;
-using System.Xml;
 
 namespace AlienRace
 {
-    public sealed class AlienPartGenerator
+    public sealed partial class AlienPartGenerator
     {
         public List<string> aliencrowntypes = new List<string>() { "Average_Normal" };
 
@@ -168,139 +167,6 @@ namespace AlienRace
                 Scribe_Values.Look(ref this.skinColorSecond, "skinColorSecondAlien");
                 Scribe_Values.Look(ref this.hairColorSecond, "hairColorSecondAlien");
                 Scribe_Values.Look(ref this.crownType, "crownType");
-            }
-        }
-
-        public class BodyAddon
-        {
-            public string path;
-            public BodyPartDef bodyPart;
-            public bool UseSkinColor = true;
-            public BodyAddonOffsets offsets;
-            public bool linkVariantIndexWithPrevious = false;
-            public float angle = 0f;
-            public bool inFrontOfBody = false;
-
-            public int variantCount = 0;
-
-            public List<BodyAddonHediffGraphic> hediffGraphics;
-            public List<BodyAddonBackstoryGraphic> backstoryGraphics;
-
-            public Mesh addonMesh;
-            public Mesh addonMeshFlipped;
-
-            public Mesh addonPortraitMesh;
-            public Mesh addonPortraitMeshFlipped;
-
-            public List<BodyPartGroupDef> hiddenUnderApparelFor = new List<BodyPartGroupDef>();
-            public List<string> hiddenUnderApparelTag = new List<string>();
-
-            public string backstoryRequirement;
-
-            public bool CanDrawAddon(Pawn pawn) => 
-                ((this.hiddenUnderApparelTag.NullOrEmpty() && this.hiddenUnderApparelFor.NullOrEmpty()) || 
-                !pawn.apparel.WornApparel.Any(ap => ap.def.apparel.bodyPartGroups.Any(bpgd => this.hiddenUnderApparelFor.Contains(bpgd)) || 
-                ap.def.apparel.tags.Any(s => this.hiddenUnderApparelTag.Contains(s)))) &&
-                    (this.backstoryRequirement.NullOrEmpty() || pawn.story.AllBackstories.Any(b=> b.identifier == this.backstoryRequirement)) && 
-                    RestUtility.CurrentBed(pawn) == null && !pawn.Downed && pawn.GetPosture() == PawnPosture.Standing && !pawn.Dead && 
-                    (this.bodyPart == null || pawn.health.hediffSet.GetNotMissingParts().Any(bpr => bpr.def == this.bodyPart));
-
-            public Graphic GetPath(Pawn pawn, ref int sharedIndex)
-            {
-                string path = "";
-                int variantCount = 0;
-
-                if (this.backstoryGraphics?.FirstOrDefault(babgs => pawn.story.AllBackstories.Any(bs => bs.identifier == babgs.backstory)) is BodyAddonBackstoryGraphic babg)
-                {
-                    path = babg.path;
-                    variantCount = babg.variantCount;
-                }else if(this.hediffGraphics?.FirstOrDefault(bahgs => pawn.health.hediffSet.hediffs.Any(h => h.def.defName == bahgs.hediff)) is BodyAddonHediffGraphic bahg)
-                {
-                    path = bahg.path;
-                    variantCount = bahg.variantCount;
-                } else
-                {
-                    path = this.path;
-                    variantCount = this.variantCount;
-                }
-                int tv;
-                return !path.NullOrEmpty() ?
-                            GraphicDatabase.Get<Graphic_Multi>(path +
-                                    ((tv = this.linkVariantIndexWithPrevious ?
-                                        sharedIndex % variantCount :
-                                        sharedIndex = Rand.Range(0, variantCount)) == 0 ? "" : tv.ToString()),
-                                ShaderDatabase.Transparent,
-                                    new Vector3(1, 0, 1),
-                                        this.UseSkinColor ?
-                                            pawn.story.SkinColor :
-                                            pawn.story.hairColor,
-                                                this.UseSkinColor ?
-                                                    (pawn.def as ThingDef_AlienRace).alienRace.generalSettings.alienPartGenerator.SkinColor(pawn, false) :
-                                                    pawn.story.hairColor) :
-                            null;
-            }
-        }
-
-        public class BodyAddonHediffGraphic
-        {
-            public string hediff;
-            public string path;
-            public int variantCount = 0;
-
-            public void LoadDataFromXmlCustom(XmlNode xmlRoot)
-            {
-                this.hediff = xmlRoot.Name;
-                this.path = xmlRoot.FirstChild.Value;
-            }
-        }
-
-        public class BodyAddonBackstoryGraphic
-        {
-            public string backstory;
-            public string path;
-            public int variantCount = 0;
-
-            public void LoadDataFromXmlCustom(XmlNode xmlRoot)
-            {
-                this.backstory = xmlRoot.Name;
-                this.path = xmlRoot.FirstChild.Value;
-            }
-        }
-
-        public class BodyAddonOffsets
-        {
-            public RotationOffset front;
-            public RotationOffset back;
-            public RotationOffset side;
-        }
-
-        public class RotationOffset
-        {
-            public List<BodyTypeOffset> bodyTypes;
-            public List<CrownTypeOffset> crownTypes;
-        }
-
-        public class BodyTypeOffset
-        {
-            public BodyType bodyType;
-            public Vector2 offset = Vector2.zero;
-
-            public void LoadDataFromXmlCustom(XmlNode xmlRoot)
-            {
-                this.bodyType = (BodyType) Enum.Parse(typeof(BodyType), xmlRoot.Name);
-                this.offset = (Vector2)ParseHelper.FromString(xmlRoot.FirstChild.Value, typeof(Vector2));
-            }
-        }
-
-        public class CrownTypeOffset
-        {
-            public string crownType;
-            public Vector2 offset = Vector2.zero;
-
-            public void LoadDataFromXmlCustom(XmlNode xmlRoot)
-            {
-                this.crownType = xmlRoot.Name;
-                this.offset = (Vector2)ParseHelper.FromString(xmlRoot.FirstChild.Value, typeof(Vector2));
             }
         }
     }
