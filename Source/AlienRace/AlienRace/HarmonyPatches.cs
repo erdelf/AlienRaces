d07ad7f3ee8020960ef432eb98cd4e8600f28b1e
erdelf <erdelf.productions@gmail.com>
2018-04-16 15:26:49 +0200

    Updated harmony

--- Source/AlienRace/AlienRace/HarmonyPatches.cs
+++ Source/AlienRace/AlienRace/HarmonyPatches.cs
@@ -1,4 +1,4 @@
-﻿using Harmony;
+﻿using HarmonyErdelf;
 using RimWorld;
 using System;
 using System.Collections.Generic;
--- Source/AlienRace/AlienRace/ScenPart_StartingHumanlikes.cs
+++ Source/AlienRace/AlienRace/ScenPart_StartingHumanlikes.cs
@@ -1,5 +1,4 @@
 ﻿using RimWorld;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
