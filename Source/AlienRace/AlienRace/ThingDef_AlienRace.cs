85a58aab5034481d045912c30e0c11f7589b9b52
erdelf <erdelf.productions@gmail.com>
2018-07-10 22:19:00 +0200

    fixed skeleton issues

--- Source/AlienRace/AlienRace/ThingDef_AlienRace.cs
+++ Source/AlienRace/AlienRace/ThingDef_AlienRace.cs
@@ -106,6 +106,7 @@ namespace AlienRace
         public Vector2 headOffset = Vector2.zero;
 
         public const string VANILLA_HEAD_PATH = "Things/Pawn/Humanlike/Heads/";
+        public const string VANILLA_SKELETON_PATH = "Things/Pawn/Humanlike/Heads/";
 
         public string body = "Things/Pawn/Humanlike/Bodies/";
         public string head = "Things/Pawn/Humanlike/Heads/";
