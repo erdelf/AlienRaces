afd8febd23eadc912b7f31ae28b7ade12393a514
erdelf <erdelf.productions@gmail.com>
2018-07-18 22:45:41 +0200

    added backstory tag insertion

--- Source/AlienRace/AlienRace/RaceSettings.cs
+++ Source/AlienRace/AlienRace/RaceSettings.cs
@@ -1,13 +1,31 @@
 ﻿namespace AlienRace
 {
     using System.Collections.Generic;
+    using System.Linq;
+    using RimWorld;
     using Verse;
 
     [StaticConstructorOnStartup]
     public class RaceSettings : Def
     {
         public PawnKindSettings pawnKindSettings;
+        public List<BackstoryTagItem> backstoryTagInsertion;
 
+        static RaceSettings()
+        {
+            foreach (BackstoryTagItem bt in DefDatabase<RaceSettings>.AllDefs.SelectMany(selector: rs => rs.backstoryTagInsertion))
+            {
+                foreach (string backstory in bt.backstories)
+                    if (BackstoryDatabase.TryGetWithIdentifier(identifier: backstory, bs: out Backstory bs))
+                        bs.spawnCategories.AddRange(collection: bt.spawnCategories);
+            }
+        }
+    }
+
+    public class BackstoryTagItem
+    {
+        public List<string> backstories     = new List<string>();
+        public List<string> spawnCategories = new List<string>();
     }
 
     public class PawnKindSettings
