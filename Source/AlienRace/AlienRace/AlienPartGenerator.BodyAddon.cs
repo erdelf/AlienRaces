3bba3854343b10d9e54ee659aaf468700cf183ea
Will Rich <williamrich@boaweb.com>
2019-01-31 18:59:03 -0600

    fix logic

--- Source/AlienRace/AlienRace/AlienPartGenerator.BodyAddon.cs
+++ Source/AlienRace/AlienRace/AlienPartGenerator.BodyAddon.cs
@@ -43,8 +43,8 @@ namespace AlienRace
                 !pawn.apparel.WornApparel.Any(predicate: ap => ap.def.apparel.bodyPartGroups.Any(predicate: bpgd => this.hiddenUnderApparelFor.Contains(item: bpgd)) || 
                 ap.def.apparel.tags.Any(predicate: s => this.hiddenUnderApparelTag.Contains(item: s)))) && (pawn.GetPosture() == PawnPosture.Standing || (pawn.InBed() && this.drawnInBed) || this.drawnOnGround) &&
                     (this.backstoryRequirement.NullOrEmpty() || pawn.story.AllBackstories.Any(predicate: b=> b.identifier == this.backstoryRequirement)) &&   
-                    (this.bodyPart.NullOrEmpty() || pawn.health.hediffSet.GetNotMissingParts().Any(predicate: bpr => bpr.untranslatedCustomLabel == this.bodyPart || bpr.def.defName == this.bodyPart)) || 
-               ( drawForFemale && drawForMale ? true : (pawn.gender == Gender.Female) ? drawForFemale : drawForMale );
+                    (this.bodyPart.NullOrEmpty() || pawn.health.hediffSet.GetNotMissingParts().Any(predicate: bpr => bpr.untranslatedCustomLabel == this.bodyPart || bpr.def.defName == this.bodyPart) || 
+               ( (pawn.gender == Gender.Female) ? drawForFemale : drawForMale ));
 
             public virtual Graphic GetPath(Pawn pawn, ref int sharedIndex, int? savedIndex = new int?())
             {
