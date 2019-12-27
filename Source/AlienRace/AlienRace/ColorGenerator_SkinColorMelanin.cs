3f115bb2137b34754d876c857cf7bf09f9bbef42
Simon Latusek <simon.latusek@gmail.com>
2019-12-27 05:48:16 +0100

    fixed human skin coloring

---
+++ Source/AlienRace/AlienRace/ColorGenerator_SkinColorMelanin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlienRace
+{
+    using JetBrains.Annotations;
+    using RimWorld;
+    using UnityEngine;
+    using Verse;
+
+    [UsedImplicitly]
+    public class ColorGenerator_SkinColorMelanin : ColorGenerator
+    {
+        public float minMelanin = 0f;
+        public float maxMelanin = 1f;
+
+        public override Color NewRandomizedColor() => 
+            PawnSkinColors.GetSkinColor(melanin: Random.Range(min: this.minMelanin, max: this.maxMelanin));
+    }
+}
