e4210c6bb9be153f15680bf7b6eabfe4cd38b339
Dexter Lowe <feldoh10@gmail.com>
2022-06-09 00:02:49 +0100

    Cleanup imports

--- Source/AlienRace/AlienRace/AlienPartGenerator.BodyAddon.cs
+++ Source/AlienRace/AlienRace/AlienPartGenerator.BodyAddon.cs
@@ -2,10 +2,7 @@ namespace AlienRace
 {
     using System.Collections.Generic;
     using System.Linq;
-    using System.Xml;
     using BodyAddonSupport;
-    using HarmonyLib;
-    using JetBrains.Annotations;
     using RimWorld;
     using UnityEngine;
     using Verse;
