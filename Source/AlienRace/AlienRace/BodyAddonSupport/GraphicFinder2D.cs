61443660a3be80b9ea4f1822820137c9a1b9cefe
Dexter Lowe <feldoh10@gmail.com>
2022-06-12 13:00:13 +0100

    Better error for texture loading failures

--- Source/AlienRace/AlienRace/BodyAddonSupport/GraphicFinder2D.cs
+++ Source/AlienRace/AlienRace/BodyAddonSupport/GraphicFinder2D.cs
@@ -1,11 +1,23 @@
 namespace AlienRace.BodyAddonSupport;
 
+using System;
 using UnityEngine;
 using Verse;
 
-class GraphicFinder2D : IGraphicFinder<Texture2D>
+internal class GraphicFinder2D : IGraphicFinder<Texture2D>
 {
-    public Texture2D GetByPath(string basePath, int variant, string direction, bool reportFailure) =>
-        ContentFinder<Texture2D>.Get($"{basePath}{(variant == 0 ? "" : variant.ToString())}_{direction}",
-                                     reportFailure);
+    public Texture2D GetByPath(string basePath, int variant, string direction, bool reportFailure)
+    {
+        string path = $"{basePath}{(variant == 0 ? "" : variant.ToString())}_{direction}";
+        try
+        {
+            return ContentFinder<Texture2D>.Get(path, reportFailure);
+        }
+        catch (Exception e)
+        {
+            string message =
+                $"Unable to load variant {variant.ToString()} for path '{basePath}' and direction {direction} (reportFailure: {reportFailure}). Tried {path}";
+            throw new Exception(message, e);
+        }
+    }
 }
