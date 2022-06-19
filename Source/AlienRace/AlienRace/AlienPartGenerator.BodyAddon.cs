441ff2342e6dbcd2e8b3453c1291e9e0e12f9089
Dexter Lowe <feldoh10@gmail.com>
2022-06-19 20:52:02 +0100

    Fix rewinding stack not exploring siblings after the first better graphic tree

--- Source/AlienRace/AlienRace/AlienPartGenerator.BodyAddon.cs
+++ Source/AlienRace/AlienRace/AlienPartGenerator.BodyAddon.cs
@@ -9,33 +9,32 @@ namespace AlienRace
 
     public partial class AlienPartGenerator
     {
-        public class BodyAddon: GenericBodyAddonGraphic
+        public class BodyAddon : GenericBodyAddonGraphic
         {
             public string bodyPart;
-            
-            public string defaultOffset = "Center";
-            [Unsaved]
-            public BodyAddonOffsets defaultOffsets;
 
-            public BodyAddonOffsets offsets = new ();
-            public bool linkVariantIndexWithPrevious = false;
-            public float angle = 0f;
-            public bool inFrontOfBody = false;
-            public bool layerInvert = true;
+            public           string           defaultOffset = "Center";
+            [Unsaved] public BodyAddonOffsets defaultOffsets;
+
+            public BodyAddonOffsets offsets                      = new();
+            public bool             linkVariantIndexWithPrevious = false;
+            public float            angle                        = 0f;
+            public bool             inFrontOfBody                = false;
+            public bool             layerInvert                  = true;
 
 
-            public bool drawnOnGround = true;
-            public bool drawnInBed = true;
+            public bool drawnOnGround   = true;
+            public bool drawnInBed      = true;
             public bool drawnDesiccated = true;
-            public bool drawForMale = true;
-            public bool drawForFemale = true;
+            public bool drawForMale     = true;
+            public bool drawForFemale   = true;
 
             public bool alignWithHead = false;
 
-            public Vector2 drawSize = Vector2.one;
-            public Vector2 drawSizePortrait = Vector2.zero;
-            public bool drawRotated = true;
-            public bool scaleWithPawnDrawsize = false;
+            public Vector2 drawSize              = Vector2.one;
+            public Vector2 drawSizePortrait      = Vector2.zero;
+            public bool    drawRotated           = true;
+            public bool    scaleWithPawnDrawsize = false;
 
             private string colorChannel;
 
@@ -48,7 +47,7 @@ namespace AlienRace
             public bool debug = true;
 
             public List<BodyPartGroupDef> hiddenUnderApparelFor = new List<BodyPartGroupDef>();
-            public List<string> hiddenUnderApparelTag = new List<string>();
+            public List<string>           hiddenUnderApparelTag = new List<string>();
 
             public string backstoryRequirement;
             public string bodyTypeRequirement;
@@ -61,103 +60,119 @@ namespace AlienRace
                 pawn.GetGender() == Gender.Female ? this.drawForFemale : this.drawForMale;
 
             private bool VisibleForBodyTypeOf(BodyAddonPawnWrapper pawn) => this.bodyTypeRequirement.NullOrEmpty() ||
-                                                             pawn.HasBodyTypeNamed(this.bodyTypeRequirement);
+                                                                            pawn.HasBodyTypeNamed(this
+                                                                            .bodyTypeRequirement);
 
             private bool VisibleUnderApparelOf(BodyAddonPawnWrapper pawn) =>
-                !pawn.HasApparelGraphics() ||
+                !pawn.HasApparelGraphics()                                                             ||
                 (this.hiddenUnderApparelTag.NullOrEmpty() && this.hiddenUnderApparelFor.NullOrEmpty()) ||
                 !pawn.GetWornApparel().Any(ap => !ap.hatRenderedFrontOfFace &&
-                                                ap.bodyPartGroups.Any(predicate: bpgd => this.hiddenUnderApparelFor.Contains(bpgd)) ||
-                                                ap.tags.Any(s => this.hiddenUnderApparelTag.Contains(s)));
-            
+                                                 ap.bodyPartGroups.Any(predicate: bpgd => this.hiddenUnderApparelFor
+                                                                       .Contains(bpgd)) ||
+                                                 ap.tags.Any(s => this.hiddenUnderApparelTag.Contains(s)));
+
             private bool VisibleForPostureOf(BodyAddonPawnWrapper pawn) =>
                 (pawn.GetPosture() == PawnPosture.Standing || this.drawnOnGround) &&
-                (pawn.VisibleInBed() || this.drawnInBed);
+                (pawn.VisibleInBed()                       || this.drawnInBed);
 
 
             private bool VisibleForBackstoryOf(BodyAddonPawnWrapper pawn) => this.backstoryRequirement.NullOrEmpty() ||
-                                                            pawn.HasBackstory(this.backstoryRequirement);
+                                                                             pawn
+                                                                             .HasBackstory(this.backstoryRequirement);
 
             private bool VisibleForRotStageOf(BodyAddonPawnWrapper pawn) =>
                 this.drawnDesiccated || pawn.GetRotStage() != RotStage.Dessicated;
 
             private bool RequiredBodyPartExistsFor(BodyAddonPawnWrapper pawn) =>
-                this.bodyPart.NullOrEmpty() ||
+                this.bodyPart.NullOrEmpty()          ||
                 pawn.HasNamedBodyPart(this.bodyPart) ||
                 (this.hediffGraphics?.Any(predicate: bahg => bahg.hediff == HediffDefOf.MissingBodyPart) ?? false);
 
             public virtual bool CanDrawAddon(Pawn pawn) => this.CanDrawAddon(new BodyAddonPawnWrapper(pawn));
+
             private bool CanDrawAddon(BodyAddonPawnWrapper pawn) =>
-                this.VisibleUnderApparelOf(pawn) &&
-                this.VisibleForPostureOf(pawn) &&
-                this.VisibleForBackstoryOf(pawn) &&
-                this.VisibleForRotStageOf(pawn) &&
+                this.VisibleUnderApparelOf(pawn)     &&
+                this.VisibleForPostureOf(pawn)       &&
+                this.VisibleForBackstoryOf(pawn)     &&
+                this.VisibleForRotStageOf(pawn)      &&
                 this.RequiredBodyPartExistsFor(pawn) &&
-                this.VisibleForGenderOf(pawn) &&
+                this.VisibleForGenderOf(pawn)        &&
                 this.VisibleForBodyTypeOf(pawn);
 
             public IBodyAddonGraphic GetBestGraphic(BodyAddonPawnWrapper pawn, string part)
             {
-                IBodyAddonGraphic bestGraphic = this;
-                Stack<IEnumerator<IBodyAddonGraphic>> stack = new Stack<IEnumerator<IBodyAddonGraphic>>();
-                stack.Push(this.GetSubGraphics(pawn, part)); // generate list of subgraphics
+                Pair<int, IBodyAddonGraphic>                     bestGraphic = new(0, this);
+                Stack<Pair<int, IEnumerator<IBodyAddonGraphic>>> stack       = new();
+                stack.Push(new Pair<int,
+                               IEnumerator<
+                                   IBodyAddonGraphic>>(1, this.GetSubGraphics(pawn, part))); // generate list of subgraphics
 
                 // Loop through sub trees until we find a deeper match or we run out of alternatives
-                while (stack.Count > 0 && bestGraphic == this)
+                while (stack.Count > 0 && (bestGraphic.Second == this || bestGraphic.First < stack.Peek().First))
                 {
-                    IEnumerator<IBodyAddonGraphic> currentGraphicSet = stack.Pop(); // get the top of the stack  
-                    while (currentGraphicSet.MoveNext()) // exits if iterates through list of subgraphics without advancing
+                    Pair<int, IEnumerator<IBodyAddonGraphic>>
+                        currentGraphicSet = stack.Pop(); // get the top of the stack
+                    while (currentGraphicSet.Second
+                                         .MoveNext()) // exits if iterates through list of subgraphics without advancing
                     {
-                        IBodyAddonGraphic current = currentGraphicSet.Current; //current branch of tree
+                        IBodyAddonGraphic current = currentGraphicSet.Second.Current; //current branch of tree
                         if (!(current?.IsApplicable(pawn, part) ?? false)) continue;
-                        if (current.GetPath() == null)
+                        if (current.GetPath().NullOrEmpty())
                             // add the current layer back to the stack so we can rewind
                             stack.Push(currentGraphicSet);
                         else
                             // Only update best graphic if the current one has a valid path
-                            bestGraphic = current;
+                            bestGraphic = new Pair<int, IBodyAddonGraphic>(currentGraphicSet.First, current);
 
-                        currentGraphicSet = current.GetSubGraphics(pawn, part); //enters next layer/branch
+                        // enters next layer/branch
+                        currentGraphicSet =
+                            new Pair<int, IEnumerator<IBodyAddonGraphic>>(currentGraphicSet.First + 1,
+                                                                          current.GetSubGraphics(pawn, part));
                     }
                 }
-                return bestGraphic;
-            }
 
+                return bestGraphic.Second;
+            }
 
             public virtual Graphic GetPath(Pawn pawn, ref int sharedIndex, int? savedIndex = new int?())
             {
-                IBodyAddonGraphic bestGraphic = this.GetBestGraphic(new BodyAddonPawnWrapper(pawn), this.bodyPart);//finds deepest match
+                IBodyAddonGraphic
+                    bestGraphic =
+                        this.GetBestGraphic(new BodyAddonPawnWrapper(pawn), this.bodyPart); //finds deepest match
 
-                string returnPath = bestGraphic.GetPath() ?? string.Empty;
+                string returnPath      = bestGraphic.GetPath() ?? string.Empty;
                 int    variantCounting = bestGraphic.GetVariantCount();
 
                 if (variantCounting <= 0)
                     variantCounting = 1;
 
                 ExposableValueTuple<Color, Color> channel = pawn.GetComp<AlienComp>().GetChannel(this.ColorChannel);
-                int tv;
+                int                               tv;
 
                 //Log.Message($"{pawn.Name.ToStringFull}\n{channel.first.ToString()} | {pawn.story.hairColor}");
 
-                return !returnPath.NullOrEmpty() ?
-                           GraphicDatabase.Get<Graphic_Multi_RotationFromData>(returnPath += (tv = (savedIndex.HasValue ? (sharedIndex = savedIndex.Value % variantCounting) :
-                                                                                             (this.linkVariantIndexWithPrevious ?
-                                                                                                  sharedIndex % variantCounting :
-                                                                                                  (sharedIndex = Rand.Range(min: 0, variantCounting))))) == 0 ? "" : tv.ToString(),
-                                                              ContentFinder<Texture2D>.Get(returnPath + "_northm", reportFailure: false) == null ? this.ShaderType.Shader : ShaderDatabase.CutoutComplex, //ShaderDatabase.Transparent,
-                                                              this.drawSize * 1.5f,
-                                                              channel.first, channel.second, new GraphicData
-                                                              {
-                                                                  drawRotated = !this.drawRotated
-                                                              }) :
-                           null;
+                return !returnPath.NullOrEmpty()
+                           ? GraphicDatabase
+                           .Get<
+                                   Graphic_Multi_RotationFromData>(returnPath += (tv = (savedIndex.HasValue ? (sharedIndex = savedIndex.Value % variantCounting) : (this.linkVariantIndexWithPrevious ? sharedIndex % variantCounting : (sharedIndex = Rand.Range(min: 0, variantCounting))))) == 0 ? "" : tv.ToString(),
+                                                                   ContentFinder<Texture2D>.Get(returnPath + "_northm",
+                                                                       reportFailure: false) == null
+                                                                       ? this.ShaderType.Shader
+                                                                       : ShaderDatabase
+                                                                       .CutoutComplex, //ShaderDatabase.Transparent,
+                                                                   this.drawSize * 1.5f,
+                                                                   channel.first, channel.second, new GraphicData
+                                                                       {
+                                                                           drawRotated = !this.drawRotated
+                                                                       })
+                           : null;
             }
 
 
             // Top level so always considered applicable
             public override bool IsApplicable(BodyAddonPawnWrapper pawn, string part) => true;
         }
-        
+
         public enum BodyAddonPrioritization : byte
         {
             Severity,
