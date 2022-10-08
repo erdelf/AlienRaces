0337ceaed10c4300e35550bd27a9c2089f7c6679
Simon Latusek <simon.latusek@gmail.com>
2022-10-08 04:04:55 +0200

    initial 1.4 update

--- Source/AlienRace/AlienRace/PawnBioDef.cs
+++ Source/AlienRace/AlienRace/PawnBioDef.cs
@@ -7,10 +7,8 @@
 
     public class PawnBioDef : Def
     {
-        public string childhoodDef;
-        public string adulthoodDef;
-        public Backstory resolvedChildhood;
-        public Backstory resolvedAdulthood;
+        public BackstoryDef childhood;
+        public BackstoryDef adulthood;
         public GenderPossibility gender;
         public NameTriple name;
         public List<ThingDef> validRaces;
@@ -18,34 +16,34 @@
         public List<string> forcedHediffs = new List<string>();
         public List<ThingDefCountRangeClass> forcedItems = new List<ThingDefCountRangeClass>();
 
+        public override IEnumerable<string> ConfigErrors()
+        {
+            if(this.childhood == null)
+                yield return "Error in " + this.defName + ": Childhood backstory not found";
+            if (this.adulthood == null)
+                yield return "Error in " + this.defName + ": Childhood backstory not found";
+
+            foreach (string configError in base.ConfigErrors())
+                yield return configError;
+        }
+
         public override void ResolveReferences()
         {
-            if (!BackstoryDatabase.TryGetWithIdentifier(this.childhoodDef, out this.resolvedChildhood))
-                Log.Error("Error in " + this.defName + ": Childhood backstory not found");
-            if (!BackstoryDatabase.TryGetWithIdentifier(this.adulthoodDef, out this.resolvedAdulthood))
-                Log.Error("Error in " + this.defName + ": Adulthood backstory not found");
-
             base.ResolveReferences();
-
-            if (this.resolvedAdulthood.slot != BackstorySlot.Adulthood || this.resolvedChildhood.slot != BackstorySlot.Childhood)
-                return;
-
+            
             PawnBio bio = new PawnBio
                           {
                               gender     = this.gender,
                               name       = this.name,
-                              childhood  = this.resolvedChildhood,
-                              adulthood  = this.resolvedAdulthood,
+                              childhood  = this.childhood,
+                              adulthood  = this.adulthood,
                               pirateKing = this.factionLeader
                           };
 
-            if (this.resolvedAdulthood.spawnCategories.Count == 1 && this.resolvedAdulthood.spawnCategories[index: 0] == "Trader")
-                this.resolvedAdulthood.spawnCategories.Add(item: "Civil");
+            if (this.adulthood.spawnCategories.Count == 1 && this.adulthood.spawnCategories[index: 0] == "Trader")
+                this.adulthood.spawnCategories.Add(item: "Civil");
 
-            if (!bio.ConfigErrors().Any())
-                SolidBioDatabase.allBios.Add(bio);
-            else
-                Log.Error(this.defName + " has errors");
+            SolidBioDatabase.allBios.Add(bio);
         }
     }
 }
