b9a84d2999ad08c4461160d1e6880900b4a2ff38
Simon Latusek <simon.latusek@gmail.com>
2022-10-28 14:06:32 +0200

    band-aid

--- Source/AlienRace/AlienRace/HarmonyPatches.cs
+++ Source/AlienRace/AlienRace/HarmonyPatches.cs
@@ -2960,6 +2960,42 @@ namespace AlienRace
                     if (ModsConfig.BiotechActive)
                     {
                         __instance.swaddledBabyGraphic = GraphicDatabase.Get<Graphic_Multi>("Things/Pawn/Humanlike/Apparel/SwaddledBaby/Swaddled_Child", ShaderDatabase.Cutout, Vector2.one, CachedData.swaddleColor(__instance));
+
+
+                        bool reinit = false;
+
+                        List<Gene> genes = alien.genes.GenesListForReading;
+
+                        int ColorDiff(Color c1, Color c2) =>
+                            (int) Math.Sqrt((c1.r - c2.r) * (c1.r - c2.r)
+                                       + (c1.g   - c2.g) * (c1.g - c2.g)
+                                       + (c1.b   - c2.b) * (c1.b - c2.b));
+                        
+                        IEnumerable<(GeneDef gd, int)> valueTuples = PawnSkinColors.SkinColorGenesInOrder.Select(gd => (gd, ColorDiff(gd.skinColorBase ?? Color.clear, alien.story.SkinColor)));
+                        (GeneDef gd, int)              valueTuple  = valueTuples.MinBy(n => n.Item2);
+
+                        Gene skinGene = genes.FirstOrDefault(g => g.def.endogeneCategory == EndogeneCategory.Melanin);
+                        if (valueTuple.gd != skinGene.def)
+                        {
+                            reinit = true;
+                            alien.genes.RemoveGene(skinGene);
+                        }
+
+                        GeneDef geneDef  = PawnHairColors.ClosestHairColorGene(alien.story.HairColor, alien.story.SkinColor);
+
+                        Gene    hairGene = genes.FirstOrDefault(g => g.def.endogeneCategory == EndogeneCategory.HairColor);
+                        if (geneDef != hairGene?.def)
+                        {
+                            reinit = true;
+                            alien.genes.RemoveGene(hairGene);
+                        }
+
+
+                        if (reinit)
+                        {
+                            alien.genes.InitializeGenesFromOldSave(valueTuple.gd.minMelanin);
+                            return false;
+                        }
                     }
                     
                     if (alien.style != null && ModsConfig.IdeologyActive && (!ModLister.BiotechInstalled || alien.genes == null || !alien.genes.GenesListForReading.Any(x => x.def.graphicData is { tattoosVisible: false } && x.Active)))
