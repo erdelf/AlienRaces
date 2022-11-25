142eb7c6528432391ad1c276b5ea9de215cc1106
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2022-11-25 17:38:13 -0600

    More child backstory functionality

--- Source/AlienRace/AlienRace/ThingDef_AlienRace.cs
+++ Source/AlienRace/AlienRace/ThingDef_AlienRace.cs
@@ -203,6 +203,7 @@
         public List<BackstoryCategoryFilter> childBackstoryFilter;
         public List<BackstoryCategoryFilter> adultBackstoryFilter;
         public List<BackstoryCategoryFilter> adultVatBackstoryFilter;
+        public List<BackstoryCategoryFilter> newbornBackstoryFilter;
     }
 
     public class FactionRelationSettings
