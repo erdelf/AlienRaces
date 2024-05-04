2b8ebe3adc3bc6dd2e99545cad7ce283e7fd883f
Simon Latusek <simon.latusek@gmail.com>
2024-05-04 14:20:15 +0200

    eating aliens precept description issue

--- Source/AlienRace/AlienRace/Thoughts.cs
+++ Source/AlienRace/AlienRace/Thoughts.cs
@@ -176,15 +176,15 @@ namespace AlienRace
 
     public class ThoughtWorker_Precept_NoRecentAlienMeat : ThoughtWorker_Precept
     {
+        private const int DAYS_WITHOUT_ALIEN_MEAT = 8;
+
         protected override ThoughtState ShouldHaveThought(Pawn p)
         {
             int num = Mathf.Max(0, p.GetComp<AlienPartGenerator.AlienComp>()?.lastAlienMeatIngestedTick ?? GenTicks.TicksGame);
-            return Find.TickManager.TicksGame - num > 8 * GenDate.TicksPerDay;
+            return Find.TickManager.TicksGame - num > DAYS_WITHOUT_ALIEN_MEAT * GenDate.TicksPerDay;
         }
 
-        public IEnumerable<NamedArgument> GetDescriptionArgs()
-        {
-            yield return 8.Named("HUMANMEATREQUIREDINTERVAL");
-        }
+        public override string PostProcessDescription(Pawn p, string description) => 
+            base.PostProcessDescription(p, description).Formatted(DAYS_WITHOUT_ALIEN_MEAT.Named("HUMANMEATREQUIREDINTERVAL"));
     }
 }
